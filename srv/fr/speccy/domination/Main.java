de2df8e4695444a967d99c6c838d26bc4e93ad53
Nicolas <nmontchamp@gmail.com>
2022-12-17 17:56:33 +0100

    update

---
+++ srv/fr/speccy/domination/Main.java
@@ -0,0 +1,14 @@
+package fr.speccy.domination;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
